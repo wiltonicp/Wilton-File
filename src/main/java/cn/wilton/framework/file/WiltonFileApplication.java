1e68b96cd1f5dbe297599a5f0c17063b59dc5b0a
mapeng <wilton.icp@gmail.com>
2021-03-12 18:06:32 +0800

    初始化

---
+++ src/main/java/cn/wilton/framework/file/WiltonFileApplication.java
@@ -0,0 +1,18 @@
+package cn.wilton.framework.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ * @author Ranger
+ * @email wilton.icp@gmail.com
+ * @since 2021/3/12
+ */
+@SpringBootApplication
+public class WiltonFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiltonFileApplication.class, args);
+    }
+}
