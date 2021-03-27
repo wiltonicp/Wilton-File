206f8356eae999b4b680c25f610dd3d7eca186fd
mapeng <wilton.icp@gmail.com>
2021-03-27 21:29:25 +0800

    静态资源初始化

---
+++ src/main/java/cn/wilton/framework/file/modules/controller/ViewController.java
@@ -0,0 +1,28 @@
+package cn.wilton.framework.file.modules.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p> 页面控制器
+ * @author Ranger
+ * @email wilton.icp@gmail.com
+ * @since 2021/3/27
+ */
+@Controller
+@RequestMapping
+public class ViewController {
+
+    @GetMapping("/login")
+    public String login(Model model){
+        return "auth-sign-in";
+    }
+
+    @GetMapping("/files")
+    public String files(Model model){
+        return "page-files";
+    }
+
+}
