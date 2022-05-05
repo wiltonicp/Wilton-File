871f91f24e2bb0c4c32574c6a126695ff04d8d35
wiltonicp <wilton.icp@gmail.com>
2022-05-05 17:24:26 +0800

    优化

--- src/main/java/cn/wilton/framework/file/modules/controller/ChatController.java
+++ src/main/java/cn/wilton/framework/file/modules/controller/ChatController.java
@@ -1,15 +1,24 @@
 package cn.wilton.framework.file.modules.controller;
 
+import cc.vihackerframework.core.api.ViHackerApiResult;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
- * <p>
- *
- * @author Ranger
- * @email wilton.icp@gmail.com
- * @since 2021/4/25
+ * Created By Ranger on 2022/5/5.
  */
+@Controller
+@RequestMapping("chat")
 public class ChatController {
 
-    public void show(){
+    @GetMapping("show")
+    public ViHackerApiResult show(){
+        return ViHackerApiResult.success();
+    }
 
+    @GetMapping("lists")
+    public ViHackerApiResult list(){
+        return ViHackerApiResult.success();
     }
 }
