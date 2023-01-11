ccd7752dc7b1e92924934c7090a09782134cac82
liming <522724933@qq.com>
2023-01-11 11:28:30 +0800

    # 计算两个时间段时间差，精确到秒

--- src/main/java/cn/wilton/framework/file/common/util/DateUtil.java
+++ src/main/java/cn/wilton/framework/file/common/util/DateUtil.java
@@ -25,6 +25,11 @@ public class DateUtil {
 
     public static final String CST_TIME_PATTERN = "EEE MMM dd HH:mm:ss zzz yyyy";
 
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    private static final long nd = 1000 * 24 * 60 * 60;
+    private static final long nh = 1000 * 60 * 60;
+    private static final long nm = 1000 * 60;
+
     public static String formatFullTime(LocalDateTime localDateTime) {
         return formatFullTime(localDateTime, FULL_TIME_PATTERN);
     }
@@ -48,5 +53,36 @@ public class DateUtil {
     public static String formatInstant(Instant instant, String format) {
         LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
         return localDateTime.format(DateTimeFormatter.ofPattern(format));
+    }
+
+    /**
+     * 计算两个时间段时间差，精确到秒
+     *
+     * @param startTime 2019-04-10 17:16:11
+     * @param endTime   2019-04-10 17:28:17
+     * @return
+     */
+    public static String computationTime(String startTime, String endTime) {
+        String str = null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date endDate = sdf.parse(endTime);
+            Date strDate = sdf.parse(startTime);
+            long diff = endDate.getTime() - strDate.getTime();
+            long day = diff / nd;
+            long hour = diff % nd / nh;
+            long min = diff % nd % nh / nm;
+            long sec = diff % nd % nh % nm / 1000;//秒
+            if (day == 0) {
+                str = hour + "小时" + min + "分钟";
+            } else if (hour == 0) {
+                str = min + "分钟";
+            } else {
+                str = day + "天" + hour + "小时" + min + "分钟";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return str;
     }
 }
